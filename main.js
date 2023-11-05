f351de4963d06925ccce699e4ce1edf98c9b75c2
UmerFaisal69 <149885078+UmerFaisal69@users.noreply.github.com>
2023-11-05 10:01:52 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const inquirer_1 = __importDefault(require("inquirer"));
+class Student {
+    constructor(n) {
+        this.name = n;
+    }
+}
+class Person {
+    constructor() {
+        this.students = [];
+    }
+    addStudent(obj) {
+        this.students.push(obj);
+    }
+}
+const persons = new Person();
+const programStart = async (persons) => {
+    do {
+        console.log("Welcome Guest !");
+        const ans = await inquirer_1.default.prompt({
+            type: "list",
+            message: "Whom Do You Want To Talk To ?",
+            name: "select",
+            choices: ["Myself", "Student"]
+        });
+        if (ans.select == "Myself") {
+            console.log("Hello I am Talking To Myself");
+            console.log("My Health is Good !");
+        }
+        if (ans.select == "Student") {
+            const ans = await inquirer_1.default.prompt({
+                type: "input",
+                message: "Which Student Do You Wanna Talk To ?",
+                name: "student"
+            });
+            const student = persons.students.find(val => val.name == ans.student);
+            if (!student) {
+                const name = new Student(ans.student);
+                persons.addStudent(name);
+                console.log(`Hello I am ${name.name} , and I am Alright !`);
+                console.log(persons.students);
+            }
+            if (student) {
+                console.log(`Hello I am ${student.name} , and I am Alright !`);
+                console.log(persons.students);
+            }
+        }
+    } while (true);
+};
+programStart(persons);
